e8aca7114224536cd94ce1feccd9e598bbef32e5
박종찬 <seheehan912@gmail.com>
2019-11-19 15:07:34 +0900

    init commit

---
+++ src/main/java/com/kakao/kakaoexam/KaKaopayExamApplication.java
@@ -0,0 +1,13 @@
+package com.kakao.kakaoexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaKaopayExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaKaopayExamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kakao/kakaoexam/KaKaopayExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kakao.kakaoexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaKaopayExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
